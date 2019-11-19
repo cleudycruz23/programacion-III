3c7cccb7ec2cf5e32ab31065edaebe22dfaae4c0
cleudy cruz <44331271+cleudycruz23@users.noreply.github.com>
2019-11-19 12:01:12 -0400

    app principal de una funcion de javaScript

---
+++ app.js
@@ -0,0 +1,15 @@
+/* SENTENCIA IF EN JAVASCRIPT*/
+
+var nombre = 'cleudy';
+var estadoCivil = 'soltero';
+
+var estaCasado = false;
+
+
+if (estadoCivil === 'casado') {
+    // si es verdadera la condicion
+    console.log(nombre + 'esta casado');
+
+} else {
+    console.log(nombre + 'esta soltero');
+}
